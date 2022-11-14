f7d73e6e69ac4244463adb19c729f2195467d831
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-11-14 16:01:22 -0500

    Moved the HomeController.cs to Controller file in Area and  models & data are deleted.

--- AnchalsBookStore/Areas/Customer/Controllers/HomeController.cs
+++ AnchalsBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AnchalsBookStore.Controllers
+namespace AnchalsBookStore.Area.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
