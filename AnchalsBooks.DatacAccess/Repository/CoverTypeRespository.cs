5d007a8df8bf4fcea58c534b518c919ba0a1f4fe
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-11-24 02:38:10 -0500

    class for coverType interface

---
+++ AnchalsBooks.DatacAccess/Repository/CoverTypeRespository.cs
@@ -0,0 +1,30 @@
+﻿using AnchalsBooks.DatacAccess.Repository.IRepository;
+using AnchalsBooks.Models;
+using AnchalsBookStore.DataAccess.Data;
+using AndrewsBooks.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AnchalsBooks.DatacAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+
+        }
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
