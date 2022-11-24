d531105df9ebe1d34195bd7eeff94dee8658a308
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-11-24 02:37:52 -0500

    interface for coverType

---
+++ AnchalsBooks.DatacAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using AnchalsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnchalsBooks.DatacAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType coverType);
+    }
+}
