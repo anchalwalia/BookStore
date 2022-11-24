525ca585d3ec6052bb55ce3781aa1dbdfba37f11
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-11-24 02:55:57 -0500

    Added new migration for AddproductToDb and created new products table

--- AnchalsBooks.DatacAccess/Migrations/20221124075347_AddProductToDb.Designer.cs
+++ AnchalsBooks.DatacAccess/Migrations/20221124075347_AddProductToDb.Designer.cs
@@ -4,14 +4,16 @@ using AnchalsBookStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AnchalsBooks.DatacAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221124075347_AddProductToDb")]
+    partial class AddProductToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
