d6a6894c3f29f63aee2b3ccffcda717cc8a9584f
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-11-24 02:38:38 -0500

    Added reference of covertype for BothUNitOfWork files

--- AnchalsBooks.DatacAccess/Repository/IRepository/IUnitOfWork.cs
+++ AnchalsBooks.DatacAccess/Repository/IRepository/IUnitOfWork.cs
@@ -7,8 +7,9 @@ namespace AnchalsBooks.DatacAccess.Repository.IRepository
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository  Category { get; }
+        ICoverTypeRepository CoverType { get; }
         ISP_Call SP_Call { get; }
 
-        void Save();
+        void save();
     }
 }
--- AnchalsBooks.DatacAccess/Repository/UnitOfWork.cs
+++ AnchalsBooks.DatacAccess/Repository/UnitOfWork.cs
@@ -14,17 +14,20 @@ namespace AnchalsBooks.DatacAccess.Repository
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            CoverType = new CoverTypeRepository(_db);
             SP_Call = new SP_Call(_db);
         }
         public ICategoryRepository Category { get; private set; }
 
         public ISP_Call SP_Call { get; private set; }
 
+        public ICoverTypeRepository CoverType { get; private set; }
+
         public void Dispose()
         {
            _db.Dispose();
         }
-        public void Save()    // all changes will be saved when the save method is called at the 'parent' level
+        public void save()
         {
             _db.SaveChanges();
         }
