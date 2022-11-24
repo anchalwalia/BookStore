55ac229cdce016169380998a32dbc99c70fff1bd
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-11-24 02:39:42 -0500

    moved covertype model class to its correct place

--- AnchalsBooks.Models/CoverType.cs
+++ AnchalsBooks.Models/CoverType.cs
@@ -5,15 +5,12 @@ using System.Text;
 
 namespace AnchalsBooks.Models
 {
-    public class Category
+    public class CoverType
     {
         [Key]
         public int Id { get; set; }
 
-        [Display(Name = "Category Name")]
         [Required]
-        [MaxLength(50)]
-
         public string Name { get; set; }
     }
 }
