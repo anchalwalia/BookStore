1f069b319f234ac5d8c08a06bc93d1bc3e67a572
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-11-24 03:21:13 -0500

    Added Upsert Method and Api calls

--- AnchalsBookStore/Areas/Admin/Controllers/ProductController.cs
+++ AnchalsBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,10 @@
 ﻿using AnchalsBooks.DatacAccess.Repository.IRepository;
+using AnchalsBooks.Models;
+using AnchalsBooks.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 
 namespace AnchalsBookStore.Areas.Admin.Controllers
 {
@@ -21,5 +25,56 @@ namespace AnchalsBookStore.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text= i.Name,
+                    Value= i.Id.ToString()
+                })
+            };
+            if (id == null)
+            {
+                return View(productVM);
+            }
+            productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            if(productVM.Product == null)
+            {
+                return NotFound();
+            }
+            return View(productVM);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category, CoverType");
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error While deleting" });
+            }
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.save();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
+        #endregion
     }
 }
