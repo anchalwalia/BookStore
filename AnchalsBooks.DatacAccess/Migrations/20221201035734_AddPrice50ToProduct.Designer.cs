2e9f9d08816382a7300b884df315fd2c949b733f
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-11-30 22:58:06 -0500

    new migration added for prices of product

--- AnchalsBooks.DatacAccess/Migrations/20221201035734_AddPrice50ToProduct.Designer.cs
+++ AnchalsBooks.DatacAccess/Migrations/20221201035734_AddPrice50ToProduct.Designer.cs
@@ -4,14 +4,16 @@ using AnchalsBookStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AnchalsBooks.DatacAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221201035734_AddPrice50ToProduct")]
+    partial class AddPrice50ToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
